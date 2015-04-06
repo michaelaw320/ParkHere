a471aa82b49eccf7c5d273daf44ea22579ae3a30
michaelaw320 <michaelaw320@gmail.com>
2015-04-06 21:55:50 +0700

    added missing file

--- ParkHere/resources/views/master_template.blade.php
+++ ParkHere/resources/views/master_template.blade.php
@@ -1,4 +1,5 @@
 Isi pake kode html buat template masternya, bagian yang changeable kosongin aja dulu kasih komentar.
 
 cara panggil bootflat dari public folder
-coba cek sini http://stackoverflow.com/questions/13433683/using-css-in-laravel-views
+
+<link rel="stylesheet" href="{{asset('bootflat/css/bootflat.min.css')}}">
